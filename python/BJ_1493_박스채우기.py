b5bd750779789a1f883965f7a4850ae9ee48a046
TobeyKwon <tobeykwon@gmail.com>
2022-03-10 21:28:58 +0900

    ps0310 python

---
+++ python/BJ_1493_박스채우기.py
@@ -0,0 +1,5 @@
+import sys; input = lambda : sys.stdin.readline().rstrip()
+
+L, W, H = map(int, input().split())
+cubes = sorted([tuple(map(int, input().split())) for _ in range(int(input()))], key=lambda x: -x[0])
+for _ in range()
---
+++ python/BJ_15688_수정렬하기5.py
@@ -0,0 +1,2 @@
+import sys; input = lambda : sys.stdin.readline().rstrip()
+print("\n".join(map(str, sorted([int(input()) for _ in range(int(input()))]))))
