ee1ddb655abbf7dc84b3f6146e49eaa0aa35f740
TobeyKwon <tobeykwon@gmail.com>
2022-03-23 00:15:23 +0900

    0323 ps python

--- python/BJ_1463_1로만들기.py
+++ python/BJ_1463_1로만들기.py
@@ -1,13 +1,14 @@
 import sys; input = lambda : sys.stdin.readline().rstrip()
 
 n = int(input())
-dp = [sys.maxsize for _ in range(n+1)]
-dp[0] = 0
+dp = [sys.maxsize] * (n + 1)
 dp[1] = 0
 
 for i in range(2, n+1):
-    if i % 2 == 0:  dp[i] = min(dp[i//2] + 1, dp[i])
-    if i % 3 == 0:  dp[i] = min(dp[i//3] + 1, dp[i])
-    dp[i] = min(dp[i-1] + 1, dp[i])
-
+    dp[i] = dp[i-1] + 1
+    if i % 3 == 0:
+        dp[i] = min(dp[i], dp[i // 3] + 1)
+    if i % 2 == 0:
+        dp[i] = min(dp[i], dp[i // 2] + 1)
+        
 print(dp[n])
