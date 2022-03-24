875da6a5749b6df94e552bfdd1ec9e89629ed550
TobeyKwon <tobeykwon@gmail.com>
2022-03-25 00:56:53 +0900

    0325 ps python

--- python/BJ_9466_텀프로젝트.py
+++ python/BJ_9466_텀프로젝트.py
@@ -1,27 +1,24 @@
 import sys; input = lambda : sys.stdin.readline().rstrip(); sys.setrecursionlimit(150000)
 
-def DFS(cur, start, check, temp):
-    if students[cur] == start:
-        return True
-    elif not check[students[cur]] or students[cur] in temp:
-        return False
+def DFS(cur, cycle, result):
+    visited[cur] = True
+    cycle.append(cur)
+    if visited[students[cur]]:
+        if students[cur] in cycle:
+            result += cycle[cycle.index(students[cur]):]
     else:
-        temp.add(students[cur])
-        return DFS(students[cur], start, check, temp)
-
-result = []
+        DFS(students[cur], cycle, result)
+        
+print_value = []
 for _ in range(int(input())):
     n = int(input())
     students = [0] + list(map(int, input().split()))
-    answer = 0
-    check = [True] * (n+1)
-    cycle = set()
+    visited = [False] * (n + 1)
+    result = []
+    
     for cur in range(1, n+1):
-        if cur not in cycle:
-            temp = set()
-            temp.add(cur)
-            if DFS(cur, cur, check, temp):  
-                cycle = cycle | temp
-                check[cur] = False
-    result.append(str(n - len(cycle)))
-print("\n".join(result))
+        if not visited[cur]:
+            cycle = []
+            DFS(cur, cycle, result)
+    print_value.append(str(n - len(result)))
+print("\n".join(print_value))
