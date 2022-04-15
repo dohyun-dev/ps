ab9fd66d8e038861e3cd7995ca4f709f57cc7263
TobeyKwon <tobeykwon@gmail.com>
2022-04-16 02:11:45 +0900

    0416 ps python

---
+++ python/bj/bfs/BJ_1697_숨바꼭질.py
@@ -0,0 +1,13 @@
+from collections import deque
+N, K = map(int, input().split())
+dist = [-1] * (100001)
+dist[N] = 0
+q = deque([N])
+while q:
+    cur = q.popleft()
+    
+    for next in [(cur-1), (cur+1), (cur*2)]:
+        if 0 <= next <= 100000 and dist[next] == -1:
+            dist[next] = dist[cur] + 1
+            q.append(next)
+print(dist[K])
