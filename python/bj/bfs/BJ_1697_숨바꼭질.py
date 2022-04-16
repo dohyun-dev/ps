889469fc588d63f8cfc2a4b5ac949bf352540995
TobeyKwon <tobeykwon@gmail.com>
2022-04-16 21:23:32 +0900

    0416 ps python

--- python/bj/bfs/BJ_1697_숨바꼭질.py
+++ python/bj/bfs/BJ_1697_숨바꼭질.py
@@ -5,7 +5,6 @@ dist[N] = 0
 q = deque([N])
 while q:
     cur = q.popleft()
-    
     for next in [(cur-1), (cur+1), (cur*2)]:
         if 0 <= next <= 100000 and dist[next] == -1:
             dist[next] = dist[cur] + 1
