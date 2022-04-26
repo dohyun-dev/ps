3b4edb2585eb8e06e7e0685833ba54963835e051
TobeyKwon <tobeykwon@gmail.com>
2022-04-27 02:28:22 +0900

    0427 python ps

---
+++ python/programmers/level2/더 맵게.py
@@ -0,0 +1,13 @@
+import heapq
+
+def solution(scoville, K):
+    answer = 0
+    heapq.heapify(scoville)
+    while len(scoville) >= 2 and scoville[0] < K:
+        heapq.heappush(scoville, heapq.heappop(scoville) + (heapq.heappop(scoville) * 2))
+        answer += 1
+    
+    if scoville[0] < K:
+        return -1
+    else:
+        return answer    
