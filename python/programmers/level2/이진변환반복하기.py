1fe06a37ffa4e24f3ef8eca3bf4cbbdfe87a8dc9
TobeyKwon <tobeykwon@gmail.com>
2022-05-08 21:44:43 +0900

    0508 ps python

---
+++ python/programmers/level2/이진변환반복하기.py
@@ -0,0 +1,19 @@
+def convert_binary(s):
+    answer = ""
+    while s != 0:
+        answer = str(s % 2) + answer
+        s //= 2
+    return answer
+        
+
+def solution(s):
+    answer = [0, 0]
+    while s != "1":
+        answer[0] += 1
+        temp = len(s)
+        s = s.replace("0", "")
+        temp -= len(s)
+        answer[1] += temp
+        s = convert_binary(len(s))
+        
+    return answer
