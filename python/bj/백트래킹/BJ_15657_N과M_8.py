d4b6cb6b5c0d58625dd4e80986bce361d8e7af9c
TobeyKwon <tobeykwon@gmail.com>
2022-05-09 01:14:36 +0900

    0509 ps python

--- python/bj/백트래킹/BJ_15657_N과M_8.py
+++ python/bj/백트래킹/BJ_15657_N과M_8.py
@@ -1,14 +1,22 @@
 import sys; input = lambda : sys.stdin.readline().rstrip()
 
-def DFS(l=0, result=[], start=0):
+
+def DFS(l=0, result=[], set_num=set()):
     if l == M:
-        print(" ".join(result), end="\n")
+        permu = " ".join(result)
+        if permu not in set_num:
+            print(permu)
+            set_num.add(permu)
         return
-    for i in range(start, N):
-        result.append(nums[i])
-        DFS(l+1, result, i)
-        result.pop()
+    for i in range(N):
+        if not check[i]:
+            check[i] = True
+            result.append(nums[i])
+            DFS(l+1, result, set_num)
+            result.pop()
+            check[i] = False
         
 N, M = map(int, input().split())
 nums = sorted(input().split(), key=lambda x: int(x))
+check = [False] * N
 DFS()
