8b01eb83bfe66421bdb0bb16a064d79679d6cd12
TobeyKwon <tobeykwon@gmail.com>
2022-05-15 01:24:28 +0900

    0515 ps python

--- python/bj/dp/BJ_9095_1,2,3더하기.py
+++ python/bj/dp/BJ_9095_1,2,3더하기.py
@@ -2,7 +2,7 @@ import sys; input = lambda : sys.stdin.readline().rstrip()
 
 for _ in range(int(input())):
     num = int(input())
-    dp = [0] * (num + 1)
+    dp = [0] * 11
     dp[1], dp[2], dp[3] = 1, 2, 4
     for i in range(4, num+1):
         dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
