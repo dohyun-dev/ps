88127385770d46520910dbae634cd27454b1d2b6
TobeyKwon <tobeykwon@gmail.com>
2022-06-21 14:18:54 +0900

    0621 ps python

--- python/bj/BJ_1253_좋다.py
+++ python/bj/BJ_1253_좋다.py
@@ -1,20 +1,19 @@
 import sys; input = lambda : sys.stdin.readline().rstrip()
-from collections import defaultdict
 
 N = int(input())
-arr = list(map(int, input().split()))
-check = defaultdict(list)
-
-for i in range(N):
-    for j in range(i+1, N):
-        check[arr[i] + arr[j]].append((i, j))
-
+arr = list(sorted(map(int, input().split())))
 answer = 0
 
-for idx, num in enumerate(arr):
-    if num in check:
-        for a, b in check[num]:
-            if idx != a and idx != b:
-                answer += 1
-                break
+for i in range(N):
+    temp = arr[:i] + arr[i+1:]
+    st, en = 0, len(temp) -1
+    while st < en:
+        sum = temp[st] + temp[en]
+        if arr[i] == sum:
+            answer += 1
+            break
+        elif arr[i] > sum:
+            st += 1
+        else:
+            en -= 1
 print(answer)
