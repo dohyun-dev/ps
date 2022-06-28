11d0306f5ae83abc6a56d5d74cc7ab0d25c8b943
TobeyKwon <tobeykwon@gmail.com>
2022-06-28 19:43:18 +0900

    0628 ps python

---
+++ python/bj/우선순위큐/BJ_2075_N번째큰수.py
@@ -0,0 +1,13 @@
+import sys; input = lambda : sys.stdin.readline().rstrip()
+from heapq import heappush, heappop, heapify
+
+N = int(input())
+arr = [[*map(int, input().split())] for _ in range(N)]
+q = [*arr[0]]
+heapify(q)
+
+for i in range(1, N):
+    for j in range(N):
+        heappush(q, arr[i][j])
+        heappop(q)
+print(heappop(q))
