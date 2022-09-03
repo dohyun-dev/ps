619fc7a9901a952b39948c4c5532154c6f5d4e2c
권도현 <tobeykwon@gmail.com>
2022-09-04 00:35:19 +0900

    0904 ps python

--- python/programmers/level2/양궁대회.py
+++ python/programmers/level2/양궁대회.py
@@ -1,56 +1,30 @@
-score_gap = 0
-result = [-1]
+from itertools import combinations_with_replacement
 
-def max_result(info, temp):
-    global result, score_gap
-    ryan = [0] * 11
-    
-    for i in temp:
-        ryan[i] += 1
-    
-    peach_score, ryan_score = 0, 0
-    for i in range(1, 11):
-        if info[i] != 0 and ryan[i] == 0:
-            peach_score += i
-        elif info[i] == 0 and ryan[i] != 0:
-            ryan_score += i
-        elif info[i] != 0 and ryan[i] != 0:
-            if info[i] >= ryan[i]:
-                peach_score += i
-            else:
-                ryan_score += i
-    if peach_score < ryan_score:
-        if len(result) == 1:
-            score_gap = ryan_score - peach_score
-            result = ryan
-        elif score_gap < ryan_score - peach_score:
-            score_gap = ryan_score - peach_score
-            result = ryan
-        elif score_gap == ryan_score - peach_score:
-            for i in range(11):
-                if result[i] > ryan[i]:
-                    return True
-                elif result[i] < ryan[i]:
-                    result = ryan
-                    break
-    return False
-
-ryan_arrow = []
-flag = False
-def dfs(N, info, pre=10, l=0):
-    global flag
-    if l == N:
-        flag = max_result(info, ryan_arrow)
-        return
-    for i in range(pre, -1, -1):
-        ryan_arrow.append(i)
-        dfs(N, info, pre, l+1)
-        ryan_arrow.pop()
-        if flag:    return
-                    
 def solution(n, info):
-    info = info[::-1]
-    dfs(n, info, 10)
-    return result[::-1]
+    result = [-1]
+    max_gap = -1
 
-print(solution(9, [0, 0, 1, 2, 0, 1, 1, 1, 1, 1, 1]))
+    for combi in combinations_with_replacement(range(10, -1, -1), n):
+        lion = [0] * 11
+
+        for i in combi:
+            lion[i] += 1
+
+        apeach_score, lion_score = 0, 0
+
+        for i in range(0, 11):
+            if info[i] == 0 and lion[i] == 0:
+                continue
+            if info[i] < lion[i]:
+                lion_score += 10 - i
+            else:
+                apeach_score += 10 - i
+
+        if apeach_score < lion_score:
+            if max_gap <= lion_score - apeach_score:
+                max_gap = lion_score - apeach_score
+                result = lion
+        print(combi)
+    return result
+
+print(solution(5, [2,1,1,1,0,0,0,0,0,0,0]))
