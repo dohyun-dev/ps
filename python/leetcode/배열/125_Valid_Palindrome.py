8f37c51db3bb3b2d195e0a2cfcd8c58c44af0b3a
권도현 <tobeykwon@gmail.com>
2022-09-25 22:28:32 +0900

    0925 파일 삭제

--- python/leetcode/배열/125_Valid_Palindrome.py
+++
@@ -1,4 +0,0 @@
-import re
-
-s = "".join(re.findall("\w|\d", input()))
-print(s == s[::-1])
--- python/leetcode/배열/238_product_of_array_except_self_못품.py
+++
@@ -1,6 +0,0 @@
-import sys; input = lambda : sys.stdin.readline().rstrip()
-
-arr = [1,2,3,4]
-mul = 1
-for i in arr:   mul *= i
-out = [mul // i for i in arr]
