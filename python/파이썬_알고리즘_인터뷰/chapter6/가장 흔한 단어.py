a9b7d68772a043d231e983683deab937a6eda357
권도현 <tobeykwon@gmail.com>
2022-09-28 19:31:54 +0900

    0928 ps

--- python/파이썬_알고리즘_인터뷰/chapter6/가장 흔한 단어.py
+++ python/파이썬_알고리즘_인터뷰/chapter6/가장 흔한 단어.py
@@ -10,5 +10,13 @@ def solution(paragraph: str, banned:list):
     return Counter(result).most_common(1)[0][0]
 
 
+# def solution(paragraph: str, banned:list):
+#     counter = defaultdict(int)
+#     for word in re.sub('[^\w]', ' ', paragraph).split():
+#         if word not in banned:
+#             counter[word] += 1
+#     return max(counter, key=counter.get)
+
+
 
 print(solution("Bob hit a ball, the hit BALL flew far after it was hit.", ["hit"]))
