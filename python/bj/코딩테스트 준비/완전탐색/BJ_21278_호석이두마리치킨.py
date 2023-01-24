2fc3bb90ba52af4c990bb45163807b7693e00c85
권도현 <tobeykwon@gmail.com>
2023-01-24 18:47:37 +0900

    0124 ps python

--- python/bj/코딩테스트 준비/완전탐색/BJ_21278_호석이두마리치킨.py
+++ python/bj/코딩테스트 준비/완전탐색/BJ_21278_호석이두마리치킨.py
@@ -1,21 +1,25 @@
 from itertools import combinations
 from collections import deque
 
-def bfs(start, shops):
-    q = deque([(start, 0)])
+def bfs(shops):
+    q = deque()
     visited = [False] * (N+1)
-    visited[start] = True
+    total = 0
+
+    for shop in shops:
+        q.append((shop, 0))
+        visited[shop] = True
 
     while q:
         cur, distance = q.popleft()
-
-        if cur in shops:
-            return distance
 
         for next in roads[cur]:
             if not visited[next]:
                 q.append((next, distance + 1))
                 visited[next] = True
+                total += distance + 1
+
+    return total * 2
 
 N, M = map(int, input().split())
 roads = {i: [] for i in range(1, N+1)}
@@ -28,9 +32,7 @@ for _ in range(M):
     roads[b].append(a)
 
 for combi in combinations(range(1, N+1), 2):
-    distance = 0
-    for i in range(1, N+1):
-        distance += bfs(i, combi) * 2
+    distance = bfs(combi)
     if distance < answer:
         answer = distance
         answer_list = combi
