c9b8480f45f62c32763f1d19ed8670a1a0e64ae0
권도현 <tobeykwon@gmail.com>
2023-01-31 18:38:38 +0900

    0131 ps python

--- python/bj/코딩테스트 준비/자료구조/BJ_14425_문자열집합.py
+++ python/bj/코딩테스트 준비/자료구조/BJ_14425_문자열집합.py
@@ -1,3 +1,9 @@
 N, M = map(int, input().split())
 arr_set = set(input() for _ in range(N))
-print(sum(1 for _ in range(M) if input() in arr_set))
+answer = 0
+
+for _ in range(M):
+    if input() in arr_set:
+        answer += 1
+
+print(answer)
