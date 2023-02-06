4a07f07b56e1d61458745b083bf03d5601acd616
권도현 <tobeykwon@gmail.com>
2023-02-06 16:35:23 +0900

    0203 ps python

--- python/bj/코딩테스트 준비/자료구조/BJ_2696_중앙값구하기.py
+++ python/bj/코딩테스트 준비/자료구조/BJ_2696_중앙값구하기.py
@@ -2,14 +2,29 @@ from heapq import heappush, heappop
 
 for t in range(int(input())):
     M, arr = int(input()), []
+    min_q, max_q = [], []
+    answer = []
 
     for _ in range(M // 10 + 1):
         arr += list(map(int, input().split()))
 
-    answer = []
-    for idx in range(1, len(arr) + 1, 2):
-        if idx % 2 == 1:
-            answer.append(str(sorted(arr[:idx])[idx // 2]))
+    for i, num in enumerate(arr, 1):
+
+
+        if len(min_q) == len(max_q):
+            heappush(min_q, num)
+        else:
+            heappush(max_q, -num)
+
+        if max_q and min_q and min_q[0] < -max_q[0]:
+            max_num = -heappop(max_q)
+            min_num = heappop(min_q)
+            heappush(max_q, -min_num)
+            heappush(min_q, max_num)
+
+        if i % 2:
+            answer.append(str(min_q[0]))
+
     print(len(answer))
     if len(answer) < 10:
         print(" ".join(answer))
