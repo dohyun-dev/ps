449ecce0d0a22dd50859d4e702e304105949ca33
권도현 <tobeykwon@gmail.com>
2023-02-16 04:03:20 +0900

    0216 ps python

--- python/bj/코딩테스트 준비/그래프 탐색/BJ_5547_일루미네이션.py
+++ python/bj/코딩테스트 준비/그래프 탐색/BJ_5547_일루미네이션.py
@@ -4,15 +4,15 @@ def dfs(x, y):
     for X, Y in d[x % 2]:
         nx, ny = x + X, y + Y
         if 0 <= nx < N+2 and 0 <= ny < M+2:
-            if map[nx][ny] == 0:
-                map[nx][ny] = 2
+            if m[nx][ny] == 0:
+                m[nx][ny] = 2
                 dfs(nx, ny)
-            if map[nx][ny] == 1:
+            if m[nx][ny] == 1:
                 cnt[0] += 1
 
-M, N = map(int, input().split())
-map = [[0] * (M + 2)] + [[0] + [*map(int, input().split())] + [0] for _ in range(N)] + [[0] * (M + 2)]
+M, N = m(int, input().split())
+m = [[0] * (M + 2)] + [[0] + [*m(int, input().split())] + [0] for _ in range(N)] + [[0] * (M + 2)]
 d = {0: [(0, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0)], 1: [(0, 1), (1, 1), (1, 0), (0, -1), (-1, 0), (-1, 1)]}
-cnt, map[0][0] = [0], 2
+cnt, m[0][0] = [0], 2
 dfs(0, 0)
 print(cnt[0])
