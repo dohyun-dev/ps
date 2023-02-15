df317ede0c5f82ac670e760a9a78c75d86ee6884
권도현 <tobeykwon@gmail.com>
2023-02-16 04:02:16 +0900

    0216 ps python

--- python/bj/코딩테스트 준비/그래프 탐색/BJ_5547_일루미네이션.py
+++ python/bj/코딩테스트 준비/그래프 탐색/BJ_5547_일루미네이션.py
@@ -1,28 +1,18 @@
-from collections import deque
+import sys; sys.setrecursionlimit(10**5)
 
-def bfs(x, y):
-    global answer
-    q = deque([(x, y)])
-    board[x][y] == "2"
-
-    while q:
-        x, y = q.popleft()
-
-        for dx, dy in dir[x%2]:
-            nx, ny = x + dx, y + dy
-            if 0 <= nx < N+2 and 0 <= ny < M+2:
-                if board[nx][ny] == "0":
-                    board[nx][ny] = "2"
-                    q.append((nx, ny))
-                elif board[nx][ny] == "1":
-                    answer += 1
+def dfs(x, y):
+    for X, Y in d[x % 2]:
+        nx, ny = x + X, y + Y
+        if 0 <= nx < N+2 and 0 <= ny < M+2:
+            if map[nx][ny] == 0:
+                map[nx][ny] = 2
+                dfs(nx, ny)
+            if map[nx][ny] == 1:
+                cnt[0] += 1
 
 M, N = map(int, input().split())
-board = [["0"] * (M+2)] + [["0"] + input().split() + ["0"] for _ in range(N)] + [["0"] * (M+2)]
-dir = {
-    0: [(0, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0)],
-    1: [(0, 1), (1, 1), (1, 0), (0, -1), (-1, 0), (-1, 1)]
-}
-answer = 0
-bfs(0, 0)
-print(answer)
+map = [[0] * (M + 2)] + [[0] + [*map(int, input().split())] + [0] for _ in range(N)] + [[0] * (M + 2)]
+d = {0: [(0, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0)], 1: [(0, 1), (1, 1), (1, 0), (0, -1), (-1, 0), (-1, 1)]}
+cnt, map[0][0] = [0], 2
+dfs(0, 0)
+print(cnt[0])
