3a38e6fc83264d095ba839d615607873e68a28bb
권도현 <tobeykwon@gmail.com>
2023-02-16 03:44:50 +0900

    0216 ps python

--- python/bj/코딩테스트 준비/그래프 탐색/BJ_5547_일루미네이션.py
+++ python/bj/코딩테스트 준비/그래프 탐색/BJ_5547_일루미네이션.py
@@ -3,28 +3,26 @@ from collections import deque
 def bfs(x, y):
     global answer
     q = deque([(x, y)])
-    visited[x][y] = True
+    board[x][y] == "2"
 
     while q:
         x, y = q.popleft()
 
-        for nx, ny in [
-            [(x, y+1), (x+1, y), (x+1, y-1), (x, y-1), (x-1, y-1), (x-1, y)],
-            [(x, y + 1), (x + 1, y + 1), (x + 1, y), (x, y - 1), (x - 1, y), (x - 1, y + 1)]
-        ][x % 2]:
+        for dx, dy in dir[x%2]:
+            nx, ny = x + dx, y + dy
             if 0 <= nx < N+2 and 0 <= ny < M+2:
-                if visited[nx][ny]:
-                    continue
-
                 if board[nx][ny] == "0":
-                    visited[nx][ny] = True
+                    board[nx][ny] = "2"
                     q.append((nx, ny))
-                else:
+                elif board[nx][ny] == "1":
                     answer += 1
 
 M, N = map(int, input().split())
 board = [["0"] * (M+2)] + [["0"] + input().split() + ["0"] for _ in range(N)] + [["0"] * (M+2)]
-visited = [[False] * (M+2) for _ in range(N+2)]
+dir = {
+    0: [(0, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0)],
+    1: [(0, 1), (1, 1), (1, 0), (0, -1), (-1, 0), (-1, 1)]
+}
 answer = 0
 bfs(0, 0)
 print(answer)
