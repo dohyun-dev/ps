5bc5e932087d13ce9548f6b26d57fe2e6a00951c
권도현 <tobeykwon@gmail.com>
2023-02-16 20:49:43 +0900

    0216 ps python

--- python/bj/코딩테스트 준비/그래프 탐색/BJ_14502_연구소.py
+++ python/bj/코딩테스트 준비/그래프 탐색/BJ_14502_연구소.py
@@ -1,4 +1,6 @@
 from collections import deque
+from itertools import combinations
+
 
 def calc():
     q = deque(virus_list)
@@ -16,23 +18,14 @@ def calc():
                 q.append((nx, ny))
     return len([1 for i in range(N) for j in range(M) if board[i][j] == "0" and not visited[i][j]])
 
-def combi(x=0, y=0, cnt=0):
-    global answer
-    if cnt == 3:
-        answer = max(answer, calc())
-        return
-    for i in range(x, N):
-        y = y if i == x else 0
-        for j in range(y, M):
-            if board[i][j] != "0":
-                continue
-            board[i][j] = "1"
-            combi(i, j+1, cnt+1)
-            board[i][j] = "0"
-
 N, M = map(int, input().split())
 board = [input().split() for _ in range(N)]
 virus_list = [(i, j) for i in range(N) for j in range(M) if board[i][j] == "2"]
 answer = 0
-combi()
+for combi in combinations([(i, j) for i in range(N) for j in range(M) if board[i][j] == "0"], 3):
+    for x, y in combi:
+        board[x][y] = "1"
+    answer = max(answer, calc())
+    for x, y in combi:
+        board[x][y] = "0"
 print(answer)
